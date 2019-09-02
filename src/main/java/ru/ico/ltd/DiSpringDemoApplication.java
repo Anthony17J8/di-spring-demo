c49575ace654120a62aa40a64abc48342dacba8d
Anton Kiselev <j8kiselev@gmail.com>
2019-09-02 20:16:10 +0300

    Spring context simple demo

--- src/main/java/ru/ico/ltd/DiSpringDemoApplication.java
+++ src/main/java/ru/ico/ltd/DiSpringDemoApplication.java
@@ -2,12 +2,18 @@ package ru.ico.ltd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.ico.ltd.controllers.MyController;
 
 @SpringBootApplication
 public class DiSpringDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiSpringDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(DiSpringDemoApplication.class, args);
+
+        MyController controller = (MyController) ctx.getBean("myController");
+
+        controller.greeting();
     }
 
 }
---
+++ src/main/java/ru/ico/ltd/controllers/MyController.java
@@ -0,0 +1,11 @@
+package ru.ico.ltd.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public void greeting() {
+        System.out.println("Hello!");
+    }
+}
