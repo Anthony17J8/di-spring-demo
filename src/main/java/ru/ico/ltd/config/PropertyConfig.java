9a97cd48c63684c739b79daced41aa9a4d4b8b4e
Anton Kiselev <j8kiselev@gmail.com>
2019-09-19 22:15:33 +0300

    Using Spring Environment Properties

--- src/main/java/ru/ico/ltd/config/PropertyConfig.java
+++ src/main/java/ru/ico/ltd/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package ru.ico.ltd.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 import ru.ico.ltd.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    private Environment env;
 
     @Value("${db.username}")
     private String username;
@@ -23,7 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(username);
+        fakeDataSource.setUsername(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
