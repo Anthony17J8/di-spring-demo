36931e694ead485132aa51488f0ede651b993ea7
Anton Kiselev <j8kiselev@gmail.com>
2019-09-20 22:14:22 +0300

    Using Spring Boot Application properties

--- src/main/java/ru/ico/ltd/config/PropertyConfig.java
+++ src/main/java/ru/ico/ltd/config/PropertyConfig.java
@@ -1,26 +1,14 @@
 package ru.ico.ltd.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import ru.ico.ltd.examplebeans.FakeDataSource;
 import ru.ico.ltd.examplebeans.FakeJmsBroker;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    private Environment env;
 
     @Value("${db.username}")
     private String username;
@@ -43,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUsername(env.getProperty("USERNAME"));
+        fakeDataSource.setUsername(username);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
@@ -56,14 +44,5 @@ public class PropertyConfig {
         fakeJmsBroker.setPassword(jmsPassword);
         fakeJmsBroker.setUrl(jmsUrl);
         return fakeJmsBroker;
-    }
-
-    //    Any BeanFactoryPostProcessor beans (This includes PropertySourcesPlaceholderConfigurer) have to be declared static.
-    //    This ensure that it is instantiated  early in the spring container life-cycle and before the containing
-    //    @Configuration class so that it can resolve @Value annotation.
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
     }
 }
